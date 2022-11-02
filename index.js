89116306f29665daaa8fc485d62a95b18ff66425
Wasif Abbas <WasifAbbas900@gmail.com>
2022-11-02 17:33:00 +0530

    new change

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 require("./DB/connection");
 const routes = require("./router/auth");
-let PORT = 80 || process.env.PORT;
+// let PORT = 80 || process.env.PORT;
 app.use(express.json());
+app.use(routes);
 app.get("/", (req, res) => {
   return res.send("HI");
 });
-app.use(routes);
 //home
 //404
 app.get("*", (req, res) => {
   res.send("err0r 404");
 });
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(` Connected at ${PORT}`);
 });
 

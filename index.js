4d6e3a862c83c772ae618452f73e1bcbe3e59cda
Wasif Abbas <WasifAbbas900@gmail.com>
2022-11-02 17:30:54 +0530

    new change

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-// const express = require("express");
-// const app = express();
-// require("./DB/connection");
-// const routes = require("./router/auth");
-// let PORT = 80 || process.env.PORT;
-// app.use(express.json());
-// app.get("/", (req, res) => {
-//   return res.send("HI");
-// });
-// app.use(routes);
-// //home
-// //404
-// app.get("*", (req, res) => {
-//   res.send("err0r 404");
-// });
-// app.listen(PORT, () => {
-//   console.log(` Connected at ${PORT}`);
-// });
-
 const express = require("express");
 const app = express();
+require("./DB/connection");
+const routes = require("./router/auth");
+let PORT = 80 || process.env.PORT;
+app.use(express.json());
 app.get("/", (req, res) => {
-  return res.send("Hi");
+  return res.send("HI");
+});
+app.use(routes);
+//home
+//404
+app.get("*", (req, res) => {
+  res.send("err0r 404");
+});
+app.listen(PORT, () => {
+  console.log(` Connected at ${PORT}`);
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  return console.log("Server started");
-});
+// const express = require("express");
+// const app = express();
+// app.get("/", (req, res) => {
+//   return res.send("Hi");
+// });
+
+// app.listen(process.env.PORT || 3000, () => {
+//   return console.log("Server started");
+// });

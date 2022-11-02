5518aede4dc3d55af1c6edb0ca1641b34b21309d
Wasif Abbas <WasifAbbas900@gmail.com>
2022-11-02 17:13:32 +0530

    new change

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 require("./DB/connection");
-const path = require("path");
 const routes = require("./router/auth");
-let port = 8000 || process.env.PORT;
+let PORT = 80 || process.env.PORT;
 app.use(express.json());
 app.use(routes);
-// app.use(express.static(path.join(__dirname,"")))
 //home
-
-//404
 app.get("/", (req, res) => {
   return res.send("HI");
 });
+//404
 app.get("*", (req, res) => {
   res.send("err0r 404");
 });
-
-app.listen(port, () => {
-  console.log(` Connected at ${port}`);
+app.listen(PORT, () => {
+  console.log(` Connected at ${PORT}`);
 });

cd59ca6a2e67427b7ec5949235bb168559b55c5c
Wasif Abbas <WasifAbbas900@gmail.com>
2022-11-03 11:18:33 +0530

    new change

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ require("./DB/connection");
 const routes = require("./router/auth");
 app.use(express.json());
 app.use(routes);
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:3000",
-  })
-);
-
-app.get("/", (req, res) => {
-  return res.send("Its running dev");
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS"
+  ),
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
 });
+app.use;
+cors({
+  credentials: true,
+  origin: "http://localhost:3000/",
+  optionsSuccessStatus: 200,
+}),
+  app.get("/", (req, res) => {
+    return res.send("Its running dev");
+  });
 
 app.listen(process.env.PORT || 8000, () => {
   return console.log("Server started");

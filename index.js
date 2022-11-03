a661a98f9eddb47a824fe461e43584c8931961bf
Wasif Abbas <WasifAbbas900@gmail.com>
2022-11-03 12:00:44 +0530

    abc

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var cors = require("cors");
 const app = express();
 require("./DB/connection");
 const routes = require("./router/auth");
-app.use(express.json());
-app.use(routes);
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -18,15 +16,19 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use;
-cors({
-  credentials: true,
-  origin: "http://localhost:3000/",
-  optionsSuccessStatus: 200,
-}),
-  app.get("/", (req, res) => {
-    return res.send("Its running dev");
-  });
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000/",
+    optionsSuccessStatus: 200,
+  })
+);
+app.use(express.json());
+
+app.use(routes);
+app.get("/", (req, res) => {
+  return res.send("Its running dev");
+});
 
 app.listen(process.env.PORT || 8000, () => {
   return console.log("Server started");

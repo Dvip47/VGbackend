19d23e005b19abbf28755e4785ddf6b2e2e5d87c
Wasif Abbas <WasifAbbas900@gmail.com>
2022-11-03 10:35:51 +0530

    abc

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ require("./DB/connection");
 const routes = require("./router/auth");
 app.use(express.json());
 app.use(routes);
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 
 app.get("/", (req, res) => {
   return res.send("Its running");
@@ -14,22 +19,3 @@ app.get("/", (req, res) => {
 app.listen(process.env.PORT || 8000, () => {
   return console.log("Server started");
 });
-
-// const express = require("express");
-// const app = express();
-// require("./DB/connection");
-// const routes = require("./router/auth");
-// let PORT = 80 || process.env.PORT;
-// app.use(express.json());
-// app.use(routes);
-// app.get("/", (req, res) => {
-//   return res.send("HI");
-// });
-// //home
-// //404
-// app.get("*", (req, res) => {
-//   res.send("err0r 404");
-// });
-// app.listen(PORT, () => {
-//   console.log(` Connected at ${PORT}`);
-// });

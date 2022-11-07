19cd763011e8e6c44500fe8ba9a26277c3774932
Wasif Abbas <WasifAbbas900@gmail.com>
2022-11-07 19:18:15 +0530

    wesite add

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS"
   ),
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", "https://vg-1.vercel.app");
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     credentials: true,
-    origin: "*",
+    origin: "https://vg-1.vercel.app",
     optionsSuccessStatus: 200,
   })
 );
